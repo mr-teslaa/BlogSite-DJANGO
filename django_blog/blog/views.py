52d199cf7bf8f8af0bf6e5cd909cdd7ce2844f96
mr-teslaa <mr.teslaa@protonmail.com>
2022-10-31 02:06:12 +0600

    finish - create delete update post

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,12 @@
+from gc import get_objects
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
 from .models import Post
 
 
@@ -7,6 +15,62 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+# remember it is class not def
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'    # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+# remember it is class not def
+class PostDetailView(DetailView):
+    model = Post
+    # in this case we don't need to specify any template 
+    # it will automatically find a template following 👇 this method
+    # <app>/<model>_<viewtype>.html
+    # in this case <app> in blog and <model> is post and <viewtype> is detail
+    # so the template name will be blog/post_detail.html
+
+
+# remember it is class not def
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# remember it is class not def
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False 
+
+
+# remember it is class not def
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False 
+
 
 def about(request):
     return render(request, 'blog/about.html', { 'title': 'About' })
--- django_blog/users/forms.py
+++ django_blog/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -12,3 +13,18 @@ class UserRegisterForm(UserCreationForm):
 
         # we neeed to tell which fields we want to shown
         fields = ['username', 'email', 'password1', 'password2']
+
+
+# we need to use ModelForm to update any database related form
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()   # you can pass reuqired=false if you want
+    
+    class Meta:
+        model = User  
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
